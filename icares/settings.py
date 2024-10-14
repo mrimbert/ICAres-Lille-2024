e72890ca352fde29cadb8ae51672b66d1ba5a808
Mathis Rimbert <mathis.rimbert@centrale.centralelille.fr>
2024-10-14 18:48:08 +0200

    debug false

--- icares/settings.py
+++ icares/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rxldufin02jl&ws(q3wn*aaz)_kn_ozz)pr=s#648rspdm5@t7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["icares.rezoleo.fr", "www.icares.rezoleo.fr", '127.0.0.1']
 
